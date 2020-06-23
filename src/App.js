af6419504b64bbcd3e271bd256be69fa9f8dd81d
WildCodeSchool <anelisynagano@gmail.com>
2020-06-23 19:16:54 +0100

    start repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Message from './components/Message';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      messages: [],
+    }
+  }
+
+  handleCount = (operator) => {
+    const counter = this.state.count;
+    if (operator === '-' && counter > 0) {
+      this.setState({ count: counter - 1 })
+    } else if (operator === '-' && counter === 0) {
+      this.setState({ count: 0 })
+    } else {
+      this.setState({ count: counter + 1 })
+    }
+  }
+
+  addMessage = () => {
+    const messages = this.state.messages;
+    this.setState({ messages: [...messages, 'Message added from state']})
+  }
+
+  render() {
+    const { count, messages } = this.state;
+    return (
+      <>
+        <div>{count}</div>
+        <button onClick={() => this.handleCount('+')}>+</button>
+        <button onClick={() => this.handleCount('-')}>-</button>
+        <button onClick={this.addMessage}>Add message</button>
+        { messages.map((message, index) => <Message key={index} message={message} />)}
+      </>
+    );
+  }
 }
 
 export default App;
